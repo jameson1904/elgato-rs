09a6d9e19eb86745a8e989b041a955933c13ea91
Jameson Papin <jamesonpapin@yahoo.com>
2026-01-01 16:04:40 -0800

    Adds a discovery command that will report found Elgato devices on the local network

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,10 @@ use std::path::PathBuf;
 use clap::{Parser, Subcommand};
 use serde::{Deserialize, Serialize};
 
+use futures_util::{pin_mut, stream::StreamExt};
+use mdns::{Error, Record, RecordKind};
+use std::{net::IpAddr, time::Duration};
+
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
 struct Cli {
@@ -13,14 +17,15 @@ struct Cli {
     config: Option<PathBuf>,
 
     #[command(subcommand)]
-    command: Option<Commands>,
+    command: Option<Command>,
 }
 
 #[derive(Subcommand)]
-enum Commands {
+enum Command {
     On,
     Off,
     Get,
+    Discover,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -35,7 +40,8 @@ struct KeyLightConfig {
     on: u8,
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let cli = Cli::parse();
     let debug_logs = cli.debug;
     println!("debug enabled: {:?}", debug_logs);
@@ -44,26 +50,31 @@ fn main() {
     }
 
     match &cli.command {
-        Some(Commands::On) => {
+        Some(Command::On) => {
             println!("Parsed command for on");
-            send_command(Commands::On);
+            handle_command(Command::On).await;
         }
-        Some(Commands::Off) => {
+        Some(Command::Off) => {
             println!("Parse command for off");
-            send_command(Commands::Off);
+            handle_command(Command::Off).await;
         }
-        Some(Commands::Get) => {
+        Some(Command::Get) => {
             println!("Parse command for get");
-            send_command(Commands::Get);
+            handle_command(Command::Get).await;
+        }
+        Some(Command::Discover) => {
+            println!("Parse command for discover");
+            handle_command(Command::Discover).await;
         }
         None => {}
     }
 }
 
-fn send_command(command: Commands) {
-    let client = reqwest::blocking::Client::new();
+async fn handle_command(command: Command) {
+    //let client = reqwest::blocking::Client::new();
+    let client = reqwest::Client::new();
     match command {
-        Commands::On => {
+        Command::On => {
             println!("Sending 'on' command to the light");
             let key_light_config = KeyLightConfig { on: 1 };
             let light_configs = vec![key_light_config];
@@ -72,14 +83,15 @@ fn send_command(command: Commands) {
                 lights: light_configs,
             };
             println!("{:?}", &key_light_command);
-            let response = client
+            let _response = client
                 .put("http://192.168.1.24:9123/elgato/lights")
                 .json(&key_light_command)
-                .send();
-            let status = response.unwrap().status();
-            println!("{}", status);
+                .send()
+                .await;
+            //let status = response.unwrap().status();
+            //println!("{}", status);
         }
-        Commands::Off => {
+        Command::Off => {
             println!("Sending 'off' command to the light");
             let key_light_config = KeyLightConfig { on: 0 };
             let light_configs = vec![key_light_config];
@@ -88,19 +100,50 @@ fn send_command(command: Commands) {
                 lights: light_configs,
             };
             println!("{:?}", &key_light_command);
-            let response = client
+            let _response = client
                 .put("http://192.168.1.24:9123/elgato/lights")
                 .json(&key_light_command)
-                .send();
-            let status = response.unwrap().status();
-            println!("{}", status);
+                .send()
+                .await;
+            //let status = response.unwrap().status();
+            //println!("{}", status);
         }
-        Commands::Get => {
+        Command::Get => {
             println!("Sending 'get' command to the light");
             // Implement the logic to send the 'off' command
-            let response = client.get("http://192.168.1.24:9123/elgato/lights").send();
-            let status = response.unwrap().status();
-            println!("{}", status);
+            let _response = client.get("http://192.168.1.24:9123/elgato/lights").send();
+            //let status = response.unwrap().status();
+            //println!("{}", status);
         }
+        Command::Discover => {
+            let _ = discover_devices().await;
+        }
+    }
+}
+
+async fn discover_devices() -> Result<(), Error> {
+    println!("Discovering Elgato Devices");
+    const SERVICE_NAME: &'static str = "_elg._tcp.local";
+    let stream = mdns::discover::all(SERVICE_NAME, Duration::from_secs(1))?.listen();
+    pin_mut!(stream);
+
+    while let Some(Ok(response)) = stream.next().await {
+        let addr = response.records().filter_map(self::to_ip_addr).next();
+
+        if let Some(addr) = addr {
+            println!("found cast device at {}", addr);
+        } else {
+            println!("cast device does not advertise address");
+        }
+    }
+
+    Ok(())
+}
+
+fn to_ip_addr(record: &Record) -> Option<IpAddr> {
+    match record.kind {
+        RecordKind::A(addr) => Some(addr.into()),
+        RecordKind::AAAA(addr) => Some(addr.into()),
+        _ => None,
     }
 }
